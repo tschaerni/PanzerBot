cc18f6335a2e2a861e321a6d354add404f068de9
Robin Cerny <robin@cerny.li>
2016-03-10 09:51:46 +0100

    fixed syntax error in line 34, removed trailing whitespaces

--- panzerbot.py
+++ panzerbot.py
@@ -12,12 +12,12 @@ sched = BlockingScheduler()
 
 def leet():
 	bot.sendMessage( sys.argv[2], "1337")
-    
+
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
     user_id = msg['user']['id']
-    
+
     zahl = random.randint(1,4)
 
     if zahl == 1:
@@ -28,12 +28,12 @@ def handle(msg):
         bot.sendMessage(chat_id, "PNAZER!11!!1!!1!!")
     elif zahl == 4:
         bot.sendMessage(chat_id, "BRUMM BRUMM!!!")
-    
+
     if command == '/source':
         bot.sendMessage(chat_id, "https://github.com/schuwima/PanzerBot/")
-    elif commane == '@PanzerBot /source':
+    elif command == '@PanzerBot /source':
         bot.sendMessage(chat_id, "https://github.com/schuwima/PanzerBot/")
-    
+
 
 TOKEN = sys.argv[1]
 	
